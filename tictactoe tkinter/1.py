cad349742e3daa6444026b04d6c2f86980fe4010
Avinash300802 <70832614+Avinash300802@users.noreply.github.com>
2022-04-26 22:34:42 +0530

    Add files via upload

---
+++ tictactoe tkinter/1.py
@@ -0,0 +1,13 @@
+# import tkinter.messagebox
+# from tkinter import *
+
+# root=Tk()
+# root.iconbitmap("tic tac toe.ico")
+# root.title("tic tac toe")
+# root.mainloop()
+# for i in range(1,100):
+#     print(i,i+i)
+import tabnanny
+a=10
+b=5
+    c=a+b
